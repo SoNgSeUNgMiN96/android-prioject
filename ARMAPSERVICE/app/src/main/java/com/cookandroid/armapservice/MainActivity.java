d00878b59b906c18c4f0aadb7625c25fe513a811
SoNgSeUNgMiN96 <tmd9544@gmail.com>
2021-11-24 02:24:55 +0900

    네이버 지도 연동 및 나침반 업데이트, AR모드로 전환할 버튼생성 및 추가, 심벌에대한 이벤트 추가

--- ARMAPSERVICE/app/src/main/java/com/cookandroid/armapservice/MainActivity.java
+++ ARMAPSERVICE/app/src/main/java/com/cookandroid/armapservice/MainActivity.java
@@ -2,6 +2,10 @@ package com.cookandroid.armapservice;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +17,7 @@ import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity  extends FragmentActivity implements OnMapReadyCallback{
@@ -26,6 +31,8 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
     private NaverMap naverMap;
     private MapView mapView;
     private FusedLocationSource locationSource;
+    Button btnAR;
+
     
 
     @Override
@@ -33,6 +40,8 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        btnAR = (Button)findViewById(R.id.ARbutton);
 
         FragmentManager fm = getSupportFragmentManager();
         MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
@@ -44,6 +53,16 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUESTS_CODE);
 
+        btnAR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(),"AR모드로 전환합니다.", Toast.LENGTH_SHORT).show();
+                /*
+                *  이 부분에 AR모드 ACTIVITY로 전환하는 코드 작성.
+                * */
+            }
+        });
 
     }
     public void onRequestPermissionsResult(int requestCode, @NonNull String [] permissions, @NonNull int [] grantResults ){
@@ -59,10 +78,21 @@ public class MainActivity  extends FragmentActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull NaverMap naverMap) {
         // ...
         this.naverMap = naverMap;
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setTiltGesturesEnabled(true);
         naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true);
         naverMap.setLocationSource(locationSource);
         naverMap.setLocationTrackingMode(LocationTrackingMode.Face);
 
+        naverMap.setOnSymbolClickListener(symbol -> {       //심벌 클릭 이벤트 리스너, 심벌의 정보는 symbol에 담겨나온다.
+
+            Toast.makeText(this, symbol.getCaption()+"의 좌표는 : "+symbol.getPosition(), Toast.LENGTH_SHORT).show();
+            return true;
+
+            // 이벤트 전파, OnMapClick 이벤트가 발생함
+        });
     }
 
 }
